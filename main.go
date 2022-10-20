b548cfe28b79e2de711f86264cff3365e83f65b1
Luis Jäger <llj.vcs@web.de>
2022-10-20 13:23:42 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"github.com/fatih/color"
+	"hash"
+	"os"
+	"strings"
+)
+
+type fn func([]string) error
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Println("usage: ch <op> [arguments...] <algorithm>")
+		os.Exit(1)
+	}
+	op := os.Args[1]
+	opTypes := make(map[string]fn, 4)
+	opTypes["check"] = check
+	opTypes["checks"] = checks
+	opTypes["create"] = create
+	opTypes["creates"] = creates
+	if _, ok := opTypes[op]; !ok {
+		fmt.Printf("unknown op: %s\n", op)
+		fmt.Println("possible values:")
+		fmt.Println("	check	- check a file against a hash")
+		fmt.Println("	checks	- check a string against a hash")
+		fmt.Println("	create	- create a hash from a file")
+		fmt.Println("	creates	- create a hash from a string")
+		os.Exit(1)
+	}
+	opFunction := opTypes[op]
+	err := opFunction(os.Args[1:])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func check(args []string) error {
+	if len(args) != 4 {
+		return errors.New("usage: ch check <file> <hash> <algorithm>")
+	}
+	file := args[1]
+	exists, err := checkIfPathIsValid(file)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		return fmt.Errorf("file %s does not exist", file)
+	}
+	fileContent, err := readFile(file)
+	if err != nil {
+		return err
+	}
+	err = checkHash(fileContent, args[2], args[3])
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func checks(args []string) error {
+	if len(args) != 4 {
+		return errors.New("usage: ch checks <string> <hash> <algorithm>")
+	}
+	err := checkHash([]byte(args[1]), args[2], args[3])
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func create(args []string) error {
+	if len(args) != 3 {
+		return errors.New("usage: ch create <file> <algorithm>")
+	}
+	file := args[1]
+	exists, err := checkIfPathIsValid(file)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		return fmt.Errorf("file %s does not exist", file)
+	}
+	fileContent, err := readFile(file)
+	if err != nil {
+		return err
+	}
+	err = createHash(fileContent, &file, args[2], true)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func creates(args []string) error {
+	if len(args) != 3 {
+		return errors.New("usage: ch creates <string> <algorithm>")
+	}
+	err := createHash([]byte(args[1]), nil, args[2], false)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func checkIfPathIsValid(path string) (bool, error) {
+	if fileInfo, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		} else {
+			return false, err
+		}
+	} else {
+		if fileInfo.IsDir() {
+			return false, fmt.Errorf("path %s is a directory, not a file", path)
+		}
+		return true, nil
+	}
+}
+
+func readFile(path string) ([]byte, error) {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	return content, nil
+}
+
+func checkHash(content []byte, hash, algorithm string) error {
+	sum, err := hashBytes(content, algorithm)
+	if err != nil {
+		return err
+	}
+	if hash != *sum {
+		color.Red("Not ok!")
+	} else {
+		color.Green("Ok!")
+	}
+	return nil
+}
+
+func createHash(content []byte, file *string, algorithm string, isFile bool) error {
+	sum, err := hashBytes(content, algorithm)
+	if err != nil {
+		return err
+	}
+	if isFile {
+		fmt.Printf("%s of %s: %s\n", color.CyanString(strings.ToUpper(algorithm)), *file, color.GreenString(*sum))
+	} else {
+		fmt.Printf("%s of \"%s\": %s\n", color.CyanString(strings.ToUpper(algorithm)), string(content), color.GreenString(*sum))
+	}
+	return nil
+}
+
+func hashBytes(content []byte, algorithm string) (*string, error) {
+	var hashCtx hash.Hash
+	switch strings.ToLower(algorithm) {
+	case "md5":
+		hashCtx = md5.New()
+	case "sha1":
+		hashCtx = sha1.New()
+	case "sha224":
+		hashCtx = sha256.New()
+	case "sha256":
+		hashCtx = sha256.New()
+	case "sha384":
+		hashCtx = sha512.New()
+	case "sha512":
+		hashCtx = sha512.New()
+	default:
+		return nil, fmt.Errorf("unknown algorithm %s", algorithm)
+	}
+	_, err := hashCtx.Write(content)
+	if err != nil {
+		return nil, err
+	}
+	sum := hex.EncodeToString(hashCtx.Sum(nil))
+	return &sum, nil
+}
